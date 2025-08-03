42967f883604f62b0647d45c0d9f91ebb9dcd96c
qeqe2029 <wowpalin46@gmail.com>
2025-08-03 21:11:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let noClickCount = 0;
+
 function no() {
-  alert("BAKIT HINDI?!?!?!?!?!");
+  noClickCount++;
+
+  if (noClickCount === 1) {
+    alert("BAKIT HINDI?!?!?!?!?!");
+  } else if (noClickCount === 2) {
+    alert("SO AYAW MO??????????????????? ");
+  } else if (noClickCount === 3) {
+    alert("Ganyanan pala ah ");
+  } else {
+    alert("EDI WAG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  }
 }
 
 function yes() {
@@ -7,4 +19,28 @@ function yes() {
   document.querySelector(".buttons").style.display = "none"; 
   const surprise = document.getElementById("surprise");
   surprise.classList.remove("hidden");
+
+  setInterval(createFlower, 300);
+}
+
+function createFlower() {
+  const flower = document.createElement("div");
+  flower.classList.add("floating-flower");
+  flower.innerText = getRandomFlower();
+
+  flower.style.left = Math.random() * 100 + "vw";
+  flower.style.animationDuration = 4 + Math.random() * 3 + "s"; // 4s–7s
+  flower.style.fontSize = 20 + Math.random() * 30 + "px";
+
+  document.body.appendChild(flower);
+
+
+  setTimeout(() => {
+    flower.remove();
+  }, 8000);
+}
+
+function getRandomFlower() {
+  const flowers = ["🌸", "🌺", "🌼", "💐", "🌷", "🌹", "🪷"];
+  return flowers[Math.floor(Math.random() * flowers.length)];
 }
